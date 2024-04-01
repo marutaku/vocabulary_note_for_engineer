ede17ad0f2d7d3240739a005dfbe669ce578cc7e
Takumi Maruyama <satouredstone@gmail.com>
2024-04-01 23:17:50 +0900

    firebaseを初期化

--- chrome-extension/src/popup/index.tsx
+++ chrome-extension/src/popup/index.tsx
@@ -1,9 +1,10 @@
 import '../global.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Popup from './popup';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div>popup</div>
+    <Popup />
   </React.StrictMode>
 );
